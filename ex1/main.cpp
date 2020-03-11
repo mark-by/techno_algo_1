a30d660804a4e26cc565b3485944eef19976eefa
Mark Bykhovets <mb@hrsmnt.ru>
2020-03-11 13:26:32 +0300

    Пройдена первая задача

--- ex1/main.cpp
+++ ex1/main.cpp
@@ -7,12 +7,12 @@ typedef struct Point {
     Point(int x, int y): x(x), y(y) {};
 } Point;
 
-float calculate_area_of_trapeze(const Point *const p1, const Point *const p2) {
-   return (float)((p1->y + p2->y)*(p1->x - p2->x))/2;
+double calculate_area_of_trapeze(const Point *const p1, const Point *const p2) {
+   return (double)((p1->y + p2->y)*(p1->x - p2->x))/2;
 }
 
-float calculate_area_of_poly(const Point *const points, const int size) {
-    float result = 0;
+double calculate_area_of_poly(const Point *const points, const int size) {
+    double result = 0;
     int i = 0;
     for (; i < size-1; i++) {
         result += calculate_area_of_trapeze(&(points[i]), &(points[i+1]));
@@ -20,7 +20,11 @@ float calculate_area_of_poly(const Point *const points, const int size) {
 
     result += calculate_area_of_trapeze(&(points[i]), points);
     
-    return result;
+    if (result < 0) {
+        return -result;
+    } else {
+        return result;
+    }
 }
 
 Point * create_poly(int * size) {
@@ -38,7 +42,6 @@ int main() {
     int size;
     Point * points = create_poly(&size);
     std::cout << calculate_area_of_poly(points, size) << std::endl;
-
     delete [] points;
     return 0;
 }
