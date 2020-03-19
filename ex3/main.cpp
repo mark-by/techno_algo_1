5fe6831431e4351ff9774595a03017a0342114a4
Mark Bykhovets <mb@hrsmnt.ru>
2020-03-20 02:25:00 +0300

    Разделены динамический массив и очереди на разные классы

--- ex3/main.cpp
+++ ex3/main.cpp
@@ -1,25 +1,48 @@
 #include <iostream>
-#include <stdio.h>
+#include <stdexcept>
+#include <algorithm>
+
+#define INITIAL_CAPACITY 7
 
 template <class T>
-class Queue {
+class DynamicArray {
     public:
-        explicit Queue(int size); 
-        ~Queue() {delete[] buffer;}
-        Queue(const Queue&); 
+        DynamicArray(): size(0), capacity(INITIAL_CAPACITY) {buffer = new T[capacity];}
+        ~DynamicArray() {delete [] buffer;}
+        DynamicArray(const DynamicArray& other);
+
+        void add(T element);
+        size_t get_size() const { return size;}
+        size_t get_capacity() const {return capacity;}
+        T& operator[] (int index);
+
+    protected:
+        size_t size;
+        size_t capacity;
+        T* buffer;
+
+        void grow();
+
+};
+
+template <class T>
+class Queue: public DynamicArray<T> {
+    public:
+        explicit Queue();
+        Queue(const Queue&);
         Queue(Queue&&) = delete;
         Queue& operator= (const Queue&) = delete;
         Queue& operator= (Queue&&) = delete;
-        
+
         void push(T element);
         T pop();
-        void realloc();
-
         bool is_empty() const {return head == tail;}
 
-private:
-        T* buffer;
-        int buffer_size;
+    private:
+        using DynamicArray<T>::capacity;
+        using DynamicArray<T>::size;
+        using DynamicArray<T>::buffer;
+        using DynamicArray<T>::grow;
         int head; //индекс первого элемент очереди
         int tail; //индекс следующего после последнего
 };
@@ -30,29 +53,61 @@ void test(Queue <T> queue);
 /*===============MAIN==========*/
 
 int main() {
-    Queue <int> q(8);
-    test(q);
+    Queue <int> queue;
+    test(queue);
     return 0;
 }
 
 /*===========================*/
 
+template <class T>
+DynamicArray<T>::DynamicArray(const DynamicArray& other) {
+    size = other.buffer;
+    capacity = other.capacity;
+    buffer = new T[capacity];
+    std::copy(other.buffer, other.buffer + capacity, buffer);
+}
 
 template <class T>
-Queue<T>::Queue(int size):
-    buffer_size(size),
-    head(0),
-    tail(0) {
-        buffer = new T[buffer_size];
+void DynamicArray<T>::add(T element) {
+    if (size != capacity) {
+        buffer[size] = element;
+        size++;
+    } else {
+        grow();
+        add(element);
     }
+}
+
+template <class T>
+void DynamicArray<T>::grow() {
+    T* temp_buffer = new T[capacity * 2];
+    std::copy(buffer, buffer + capacity, temp_buffer);
+    delete [] buffer;
+    buffer = temp_buffer;
+    capacity *= 2;
+}
+
+template <class T>
+T& DynamicArray<T>::operator[](int index) {
+    return buffer[index];
+}
+
+template <class T>
+Queue<T>::Queue(): head(0), tail(0) {}
 
 template <class T>
 void Queue<T>::push(T element) {
-    if ((tail + 1) % buffer_size == head) {
-        realloc();
+    if ((tail + 1) % capacity == head) {
+        grow();
+        if (tail < head) {
+            std::copy(buffer, buffer + tail, buffer + capacity/2);
+            tail = head + size;
+        }
     }
     buffer[tail] = element;
-    tail = (tail + 1) % buffer_size;
+    tail = (tail + 1) % capacity;
+    size++;
 }
 
 template <class T>
@@ -61,31 +116,13 @@ T Queue<T>::pop() {
         return -1;
     }
     T result = buffer[head];
-    head = (head + 1) % buffer_size;
+    head = (head + 1) % capacity;
+    size--;
     return result;
 }
 
 template <class T>
-void Queue<T>::realloc() {
-    T * temp = new T[buffer_size * 2];
-    int i = 0;
-    for (; !is_empty(); i++) {
-        temp[i] = pop();
-    }
-    head = 0;
-    tail = i;
-    delete [] buffer;
-    buffer = temp;
-    buffer_size *= 2;
-}
-
-template <class T>
 Queue<T>::Queue(const Queue& other) {
-    buffer_size = other.buffer_size;
-    buffer = new T[buffer_size];
-    for (int i = 0; i < buffer_size; i++) {
-        buffer[i] = other.buffer[i];
-    }
     head = other.head;
     tail = other.tail;
 }
@@ -93,13 +130,13 @@ Queue<T>::Queue(const Queue& other) {
 template <class T>
 void test(Queue <T> queue) {
     int in_a, in_b, n;
-    scanf("%d", &n);
+    std::cin >> n;
     for (int i = 0; i < n; i++) {
-        scanf("%d %d", &in_a, &in_b);
+        std::cin >> in_a >> in_b;
         switch (in_a) {
             case 2:
                 if (queue.pop() != in_b) {
-                    printf("NO\n");
+                    std::cout << "NO\n" << std::endl;
                     return;
                 }
                 break;
@@ -108,5 +145,5 @@ void test(Queue <T> queue) {
                 break;
         }
     }
-    printf("YES\n");
+    std::cout << "YES\n" << std::endl;
 }
