cd292774356d2d5d7e748cbca3f2f288c3297913
Mark Bykhovets <mb@hrsmnt.ru>
2020-03-27 11:13:46 +0300

    Конест пройден

--- ex4/main.cpp
+++ ex4/main.cpp
@@ -17,6 +17,7 @@ public:
     size_t get_capacity() const {return capacity;}
     T pop_back();
     T pop_first();
+    T& last() { return buffer[size - 1];}
     bool is_empty() const {return size == 0; }
     T& operator[] (int index);
 
@@ -29,6 +30,13 @@ protected:
 };
 
 template <class T>
+struct DefaultComparator {
+    bool operator() (const T& l, const T& r) {
+        return l < r;
+    }
+};
+
+template <class T, class Comparator = DefaultComparator<T>>
 class Heap : public DynamicArray<T> {
 public:
     explicit Heap() = default;
@@ -47,17 +55,22 @@ private:
     void sift_down(size_t i);
     void sift_down();
     void sift_up();
+    Comparator comp = Comparator();
+    bool lt(const T& l, const T& r);
+    bool gt(const T& l, const T& r);
+    bool eq(const T&, const T& r);
     using DynamicArray<T>::pop_back;
     using DynamicArray<T>::capacity;
     using DynamicArray<T>::size;
     using DynamicArray<T>::buffer;
     using DynamicArray<T>::grow;
     using DynamicArray<T>::operator[];
+    using DynamicArray<T>::last;
 };
 
 struct input_data {
     int amount;
-    DynamicArray<int> fruct_mass;
+    DynamicArray<int> fruits_mass;
     int carrying;
     input_data() {
         amount = 0;
@@ -67,50 +80,84 @@ struct input_data {
 
 void input(input_data &data);
 
+int count_attemps(const DynamicArray<int>& fruits, int carrying);
+
 
 // MAIN
 
 int main(int args, char ** argv) {
-//    input_data i_data;
-//    input(i_data);
-    int array[] = {5,6,8,3,6,3,8,4,5,6};
-    Heap<int> heap(array, 10);
-    for (int i = 0; i < 10; i++) {
-        std::cout << heap.pop() << std::endl;
-    }
+    input_data i_data;
+    input(i_data);
+    std::cout << count_attemps(i_data.fruits_mass, i_data.carrying) << std::endl;
     return 0;
 }
 
 // END MAIN
 
 
+int count_attemps(const DynamicArray<int>& fruits, int carrying) {
+    Heap<int> basket(fruits); // корзина
+    DynamicArray<int> pockets; // карманы
+    int attemps = 0; // количество попыток
+    for (; !basket.is_empty(); attemps++) {
+        int curr_carrying = 0;
+        while( !basket.is_empty() && (curr_carrying + basket.top()) <= carrying ) {
+            curr_carrying += basket.top();
+            pockets.add(basket.pop());
+        }
+        while( !pockets.is_empty() ) {
+            if (pockets.last() != 1) {
+                basket.add(pockets.pop_back() / 2);
+            } else {
+                pockets.pop_back();
+            }
+        }
+    }
+    return attemps;
+}
+
 // HEAP REALIZATION
 
-template <class T>
-Heap<T>::Heap(const T * array, size_t arr_size) : DynamicArray<T>(array, arr_size) {
+template <class T, class Comparator>
+Heap<T, Comparator>::Heap(const T * array, size_t arr_size) : DynamicArray<T>(array, arr_size) {
     sift_down();
 }
 
-template <class T>
-Heap<T>::Heap(const DynamicArray<T>& array) : DynamicArray<T>(array) {
+template <class T, class Comparator>
+Heap<T, Comparator>::Heap(const DynamicArray<T>& array) : DynamicArray<T>(array) {
     sift_down();
 }
 
-template <class T>
-void Heap<T>::add(const T& element) {
+template <class T, class Comparator>
+bool Heap<T, Comparator>::lt(const T& l, const T& r) {
+    return comp(l, r);
+}
+
+template <class T, class Comparator>
+bool Heap<T, Comparator>::eq(const T& l, const T& r) {
+    return (!comp(l, r) && !comp(r, l));
+}
+
+template <class T, class Comparator>
+bool Heap<T, Comparator>::gt(const T& l, const T& r) {
+    return comp(r, l);
+}
+
+template <class T, class Comparator>
+void Heap<T, Comparator>::add(const T& element) {
     DynamicArray<T>::add(element);
     sift_up();
 }
 
-template <class T>
-void Heap<T>::sift_down(size_t index) {
+template <class T, class Comparator>
+void Heap<T, Comparator>::sift_down(size_t index) {
     size_t left = index * 2 + 1;
     size_t right = index * 2 + 2;
     size_t largest = index;
-    if (left < size && buffer[left] > buffer[largest]) {
+    if (left < size && gt(buffer[left], buffer[largest])) {
         largest = left;
     }
-    if (right < size && buffer[right] > buffer[largest]) {
+    if (right < size && gt(buffer[right], buffer[largest])) {
         largest = right;
     }
 
@@ -120,19 +167,19 @@ void Heap<T>::sift_down(size_t index) {
     }
 }
 
-template <class T>
-void Heap<T>::sift_down() {
+template <class T, class Comparator>
+void Heap<T, Comparator>::sift_down() {
     for (int i = size / 2 - 1; i >= 0; i--) {
         sift_down(i);
     }
 }
 
-template<class T>
-void Heap<T>::sift_up() {
+template<class T, class Comparator>
+void Heap<T, Comparator>::sift_up() {
     size_t index = size - 1;
     while (index > 0) {
         size_t parent = (index - 1) / 2;
-        if (buffer[index] <= buffer[parent]) {
+        if (!gt(buffer[index], buffer[parent])) {
             return;
         }
         std::swap(buffer[index], buffer[parent]);
@@ -140,8 +187,8 @@ void Heap<T>::sift_up() {
     }
 }
 
-template <class T>
-T Heap<T>::pop() {
+template <class T, class Comparator>
+T Heap<T, Comparator>::pop() {
     if (is_empty()) {
         throw std::runtime_error("Heap is empty");
     }
@@ -155,8 +202,8 @@ T Heap<T>::pop() {
     return result;
 }
 
-template <class T>
-const T& Heap<T>::top() const {
+template <class T, class Comparator>
+const T& Heap<T, Comparator>::top() const {
     if (is_empty()) {
         throw std::runtime_error("Heap is empty");
     }
@@ -171,7 +218,7 @@ const T& Heap<T>::top() const {
 
 template <class T>
 DynamicArray<T>::DynamicArray(const DynamicArray& other) {
-    size = other.buffer;
+    size = other.size;
     capacity = other.capacity;
     buffer = new T[capacity];
     std::copy(other.buffer, other.buffer + capacity, buffer);
@@ -245,7 +292,7 @@ void input(input_data &data) {
     int temp;
     for (int i=0; i < data.amount; i++ ) {
         std::cin >> temp;
-        data.fruct_mass.add(temp);
+        data.fruits_mass.add(temp);
     }
     std::cin >> data.carrying;
 }
